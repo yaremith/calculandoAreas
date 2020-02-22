55efe0a86669f1381f6a701e4e9150151828ec21
Yaremith  Damian <laudamian27@gmail.com>
2020-02-22 13:10:51 -0600

    Initial Commit

---
+++ calculandoAreas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculandoAreas
+//
+//  Created by Laura Yaremith Damian  Padilla on 22/02/20.
+//  Copyright © 2020 Laura Yaremith Damian  Padilla. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calculandoAreasTests/calculandoAreasTests.swift
@@ -0,0 +1,34 @@
+//
+//  calculandoAreasTests.swift
+//  calculandoAreasTests
+//
+//  Created by Laura Yaremith Damian  Padilla on 22/02/20.
+//  Copyright © 2020 Laura Yaremith Damian  Padilla. All rights reserved.
+//
+
+import XCTest
+@testable import calculandoAreas
+
+class calculandoAreasTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
